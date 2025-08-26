b242f7c7d9b92895669a3b33f1cb09e91a1a4885
SelcukOzdemir23 <s_05_60@hotmail.com>
2025-08-26 15:16:46 +0300

    UPDATE: tekarr

--- scraper.py
+++ scraper.py
@@ -60,6 +60,9 @@ class GoogleMapsScraper:
         chrome_options = Options()
         if headless:
             chrome_options.add_argument("--headless")
+            chrome_options.add_argument("--no-sandbox")
+            chrome_options.add_argument("--disable-dev-shm-usage")
+            chrome_options.add_argument("--disable-gpu")
         
         chrome_options.add_argument("--window-size=1920,1080")
         chrome_options.add_argument("--disable-notifications")
